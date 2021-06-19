55aef1491a4f62c8dd45216e1e463a0dc3221622
momoka-mero <momokakj8.love@gmail.com>
2021-06-19 14:54:51 +0900

    アプリのベースを作った

--- todo/admin.py
+++ todo/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.utils import timezone
-from .models import Folder
+from .models import Folder, Task
 
 admin.site.register(Folder)
+admin.site.register(Task)
 
 # Register your models here.
